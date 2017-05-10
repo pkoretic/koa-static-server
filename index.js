9c7b3db10cfe1259da503745a9c5da9e68d53de6
DABH <DABH@users.noreply.github.com>
2017-05-10 15:45:59 -0700

    Convert to Koa v2.x middleware style

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * Module dependencies
  */
 
+const assert = require('assert')
 const send = require('koa-send')
 const normalize = require('path').normalize
 const path = require('path')
@@ -21,58 +22,57 @@ module.exports = serve
  * @api public
  */
 
-function serve(opts) {
+async function serve (opts) {
+    assert(typeof options.rootDir === 'string', 'rootDir must be specified (as a string)')
 
     let options = opts || {}
     options.root = path.resolve(options.rootDir || process.cwd())
     options.index = options.index || "index.html"
-    let log = options.log || false
+    const log = options.log || false
 
-    if (typeof options.rootDir !== 'string')
-        throw Error('rootDir must be specified')
-
-    return function*(next) {
+    return async (ctx, next) => {
         /* Serve folder as root path - default
          eg. for options
             rootDir = 'web'
         'web/file.txt' will be served as 'http://host/file.txt'
         */
+        assert(ctx, 'koa context requried')
         let path = this.path
         if (!options.rootPath) {
             log && console.log(new Date().toISOString(), path)
-            const sent = yield send(this, path, options)
+            const sent = await send(ctx, path, options)
             if (sent)
                 return
             else
-                return yield *next
+                return next()
         }
-
+    
         // Check if request path (eg: /doc/file.html) is allowed (eg. in /doc)
         if (path.indexOf(options.rootPath) !== 0)
-            return yield *next
-
+            return next()
+    
         /* Serve folders as specified
          eg. for options:
             rootDir = 'web/static'
             rootPath = '/static'
-
+    
         'web/static/file.txt' will be served as 'http://server/static/file.txt'
         */
-
+    
         // Redirect non-slashed request to slashed
         // eg. /doc to /doc/
-
+    
         if (path === options.rootPath)
             return this.redirect(normalize(options.rootPath + "/"))
-
+    
         if (options.rootPath)
             path = normalize(path.replace(options.rootPath, "/"))
-
+    
         log && console.log(new Date().toISOString(), path)
-        const sent = yield send(this,  path, options)
+        const sent = await send(ctx, path, options)
         if (sent)
             return
         else
-            return yield *next
+            return next()
     }
 }
