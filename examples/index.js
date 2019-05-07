4e5b7b4e777a1acd5ab2d83da03df111f2642c9a
Petar Koretic <petar.koretic@gmail.com>
2019-05-07 17:30:08 +0800

    Add the feature of refusing the request of files in the specilzed folder and refusing request folder directly

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,15 @@
 'use strict'
 
 var serve = require('..')
-var app = new (require('koa'))()
+var Koa = require('koa')
+var app = new Koa()
 
 // root index support
 // GET /
 // returns index.html
 // GET /file.txt
 // returns file.txt
-app.use(serve({rootDir: 'web'}))
+app.use(serve({rootDir: 'web', forbidden :['forbidden']}))
 
 app.listen(3000)
 
