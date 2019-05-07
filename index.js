8e254e48ae7279e3cdc796f606e6f93aad1e3a67
Jiwen Yang <youngjiwen@gmail.com>
2019-05-07 17:30:08 +0800

    Add the feature of refusing the request of files in the specilzed folder and refusing request folder directly

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 const assert = require('assert')
 const send = require('koa-send')
 const normalize = require('upath').normalizeSafe
-const path = require('path')
+const join = require('path').join;
+const resolve = require('path').resolve;
+const fs = require('fs').promises;
 
 module.exports = serve
 
@@ -26,27 +28,31 @@ function serve(opts) {
     assert(typeof opts.rootDir === 'string', 'rootDir must be specified (as a string)')
 
     let options = opts || {}
-    options.root = path.resolve(options.rootDir || process.cwd())
+    options.root = resolve(options.rootDir || process.cwd())
     options.index = options.index || "index.html"
     const log = options.log || false
     const rootPath = normalize(options.rootPath ? options.rootPath + "/" : "/")
+    const forbidden = opts.forbidden || [];
 
     return async (ctx, next) => {
         assert(ctx, 'koa context required')
 
         // skip if this is not a GET/HEAD request
-        if (ctx.method !== 'HEAD' && ctx.method !== 'GET')
+        if (ctx.method !== 'HEAD' && ctx.method !== 'GET') {
             return next()
+        }
 
         let path = ctx.path
 
         // Redirect non-slashed request to slashed, eg. /doc to /doc/
-        if (path + '/' === rootPath)
+        if (path + '/' === rootPath) {
             return ctx.redirect(rootPath)
+        }
 
         // Check if request path (eg: /doc/file.html) is allowed (eg. in /doc)
-        if (path.indexOf(rootPath) !== 0)
+        if (path.indexOf(rootPath) !== 0) {
             return next()
+        }
 
         /* Serve folders as specified
          eg. for options:
@@ -58,6 +64,34 @@ function serve(opts) {
 
         path = normalize(path.replace(rootPath, "/"))
 
+        /**
+         * If folder is in forbidden list,refuse request
+         */
+        if (forbidden.length > 0) {
+            for (let folder of forbidden) {
+                folder = new RegExp(`\/${folder}\/`, 'i');
+                if (folder.test(path)) {
+                    ctx.status = 403;
+                    ctx.body = "FORBIDDEN FOLDER"
+                    return false;
+                }
+            }
+        }
+
+        /**
+         * If someone trying to visit a folder, refust him or her
+         */
+        let CurrentPath = join(opts.rootDir, path); 
+        const stat = await fs.stat(CurrentPath);
+        if(stat.isDirectory()){
+            ctx.status = 403;
+            ctx.body = "IT IS NOT ALLOWED TO VISIT FOLDER"
+            return false;
+        }
+
+        /**
+         * LOG
+         */
         log && console.log(new Date().toISOString(), path)
 
         let sent
@@ -67,16 +101,18 @@ function serve(opts) {
         */
         try {
             sent = await send(ctx, path, options)
-        } catch (e) {
-            if (!options.notFoundFile)
-                throw e;
-
-            sent = await send(ctx, options.notFoundFile, options)
+        } catch (error) {
+            if (!options.notFoundFile) {
+                ctx.throw(404);
+            } else {
+                sent = await send(ctx, options.notFoundFile, options)
+            }
         }
 
-        if (sent && options.last !== false)
+        if (sent && options.last !== false) {
             return
-        else
+        } else {
             return next()
+        }
     }
 }
